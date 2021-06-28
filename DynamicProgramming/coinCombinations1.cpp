c9ae5de34300e2995ec972623d753213ce96a867
Anshul Mehta <72487310+AnshulMehta1@users.noreply.github.com>
2021-06-28 21:32:53 +0530

    Update coinCombinations1.cpp

--- DynamicProgramming/coinCombinations1.cpp
+++ DynamicProgramming/coinCombinations1.cpp
@@ -4,18 +4,25 @@
 //  We need to find the number of ways we can reachh the target 
 //  Input n,W --> Number of Items and Weight constraint
 //  Second Line is the value of the coins 
+// Coin Combinations
+// Author : Anshul Mehta
+//  A list of items is given with their weights and a Target Constraint is given as well
+//  We need to find the number of ways we can reachh the target 
+//  Input n,W --> Number of Items and Weight constraint
+//  Second Line is the value of the coins 
 
 
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define repetition(i, a, b) for(int i = a; i < (b); ++i)
 #define trav(a, x) for(auto& a : x)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
-typedef long long ll;
+#define ll long long 
+#define vi vector<int>
 typedef pair<int, int> pii;
-typedef vector<int> vi;
+// typedef vector<int> vi;
 
 int main() {
     cin.sync_with_stdio(0); cin.tie(0);
@@ -26,12 +33,12 @@ int main() {
     cin >> n >> x;
     vi c(n), dp(x+1);
     dp[0]=1;
-    rep(i, 0, n) cin >> c[i];
+    repetition(i, 0, n) cin >> c[i];
     auto add=[&](int &x, int a) {
         x+=a;
         if (x>=P) x-=P;
     };
-    rep(i, 0, x+1) trav(a, c)
+    repetition(i, 0, x+1) trav(a, c)
     if (i>=a) add(dp[i], dp[i-a]);
     cout << dp[x] << '\n';
     return 0;
