35bf8154bca02d0aa23c664a64bd59ccb423b951
Anshul Mehta <72487310+AnshulMehta1@users.noreply.github.com>
2021-06-28 21:29:26 +0530

    Update coinCombinations1.cpp

--- DynamicProgramming/coinCombinations1.cpp
+++ DynamicProgramming/coinCombinations1.cpp
@@ -4,44 +4,35 @@
 //  We need to find the number of ways we can reachh the target 
 //  Input n,W --> Number of Items and Weight constraint
 //  Second Line is the value of the coins 
+
+
 #include <bits/stdc++.h>
-#define ar array
-#define ll long long 
 using namespace std;
-int main(){
-    ll n;
-    ll W;
-    cin>>n>>W;
-    int max=100;
-    int c[max];
-    int mod=1e9+7;
-    int dp[n+1][W+1];
-    for (int i=0;i<n;i++){
-        cin>>c[i];
-    }
 
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
 
-    // Dynamic Programming 
-    for (int i=1;i<=n;i++){
-        for (int j=0;j<=W;j++){
-            if (j==0){
-                dp[i][j]=1;
-            }
-            else{
-                int a=(c[i]>j)?0:dp[i][j-c[i]];
-                int b=(i==1)?0:dp[i-1][j];
-                // dp[i][j]=(dp[i][j-c[i]]+dp[i-1][j])%mod;
-                dp[i][j]=(a+b)%mod;
-
-            }
-        }
-       
-
-}
-
-        cout<<dp[n][W];
-        
+int main() {
+    cin.sync_with_stdio(0); cin.tie(0);
+    cin.exceptions(cin.failbit);
     
-
-
+    constexpr int P=1e9+7;
+    int n, x;
+    cin >> n >> x;
+    vi c(n), dp(x+1);
+    dp[0]=1;
+    rep(i, 0, n) cin >> c[i];
+    auto add=[&](int &x, int a) {
+        x+=a;
+        if (x>=P) x-=P;
+    };
+    rep(i, 0, x+1) trav(a, c)
+    if (i>=a) add(dp[i], dp[i-a]);
+    cout << dp[x] << '\n';
+    return 0;
 }
