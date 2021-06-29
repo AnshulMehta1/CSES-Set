f6d3542cd163e45b5da46373b0a568964777277f
Anshul Mehta <72487310+AnshulMehta1@users.noreply.github.com>
2021-06-29 21:54:17 +0530

    Create DistinctNumber.cpp

---
+++ SortingAndSearching/DistinctNumber.cpp
@@ -0,0 +1,21 @@
+//  Restaurant Customers
+// Author: Anshul Mehta
+// A sequence is given and then need to find out the Distinct/Unique items 
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long 
+using namespace std;
+
+int main()
+{
+    ll n;
+    cin>>n;
+    set<int> s;
+    for (int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        s.insert(x);
+
+    }
+    cout << s.size() << endl;
+}
